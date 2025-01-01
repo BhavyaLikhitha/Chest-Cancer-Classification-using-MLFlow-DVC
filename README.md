12571723ea9141bbd6c793cdff8b266b934a9710
bhavyalikhitha7 <bukka.b@northeastern.edu>
2025-01-01 14:37:38 -0500

    model evaluation and prediction done, docker file creating working on ec2 machine aws

--- README.md
+++ README.md
@@ -18,3 +18,14 @@ after this do
 conda activate cancer
 pip install -r requirements.txt
 
+export/set MLFLOW_TRACKING_URI=https://dagshub.com/BhavyaLikhitha/Chest-Cancer-Classification-using-MLFlow-DVC.mlflow
+export set MLFLOW_TRACKING_USERNAME=BhavyaLikhitha \
+export set MLFLOW_TRACKING_PASSWORD =67e311e47e6329264788b16fb4d2dc2a3c432f6b \
+python script.py
+
+
+dvc init
+dvc repro
+
+
+amazon uri = 509399636936.dkr.ecr.us-east-2.amazonaws.com/chestcancer
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ChestCancerClassification import logger
 from ChestCancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChestCancerClassification.pipeline.stage_02_prepare_model import PrepareBaseModelTrainingPipeline
 from ChestCancerClassification.pipeline.stage_03_model_train import ModelTrainingPipeline
-
+from ChestCancerClassification.pipeline.stage_04_model_evaluation_mlflow import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -33,6 +33,18 @@ try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
