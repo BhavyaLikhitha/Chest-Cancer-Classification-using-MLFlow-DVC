ce8485cdc3bf60ca8b890896bff5ad9f5e89d439
bhavyalikhitha7 <bukka.b@northeastern.edu>
2024-12-31 20:23:04 -0500

    Successfully trained model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from ChestCancerClassification import logger
 from ChestCancerClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChestCancerClassification.pipeline.stage_02_prepare_model import PrepareBaseModelTrainingPipeline
+from ChestCancerClassification.pipeline.stage_03_model_train import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -21,6 +22,17 @@ try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
